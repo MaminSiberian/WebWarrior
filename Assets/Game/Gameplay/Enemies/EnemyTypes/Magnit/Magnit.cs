36ae19a644f4e0e2ddd3957b4aeedcb776a6cb69
Co6op13 <Co6op13@yandex.ru>
2023-10-25 21:36:58 +0500

    magnit

--- Assets/Game/Gameplay/Enemies/EnemyTypes/Magnit/Magnit.cs
+++ Assets/Game/Gameplay/Enemies/EnemyTypes/Magnit/Magnit.cs
@@ -1,50 +1,49 @@
 using HookControl;
+using NaughtyAttributes;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Magnit : MonoBehaviour
 {
+    [Header("Patrol point TRANSFORM")]
+    [SerializeField] private List<Transform> patrolPoints;
     [Header("Force to throw Object")]
-    [Range(0f, 100f)] [SerializeField] private float forceToPushObject;
+    [Range(0f, 100f)][SerializeField] private float forceToPushObject;
+    [SerializeField] private float timeStunEnemy;
     [SerializeField] private AnimationCurve forceCurve;
-    [Header("Âuration of force action")]
+    [Header("Duration of force action")]
     [SerializeField] private float duration;
     [SerializeField] bool StopMovingAfterFinished = true;
-
+    [Header("Need to desynchronize with others")]
     [Header("if you need to synchronize, then set the same offset")]
-    [Range(0f, 1f)] [SerializeField] private float deltaTime;
+    [Range(0f, 1f)][SerializeField] private float deltaTime;
     [Header("Loop or PingPong ")]
     [SerializeField] private bool isPigPong;
     [Header("Changes the initial direction  ")]
-    [SerializeField] private bool inverseDirectrion;
+    [SerializeField, ShowIf("isPigPong")] private bool inverseDirectrion;
     [Header("Behavior  ")]
     [SerializeField] private AnimationCurve moveCurve;
     [Header(" speed movement  ")]
-    [Range(0f, 3f)] [SerializeField] private float speedMove;
+    [Range(0f, 3f)][SerializeField] private float speedMove;
     [SerializeField] private float referenceLength;
-    [Header("Patrol point TRANSFORM")]
-    [SerializeField] private List<Transform> patrolPoints;
-    [Header("Need to desynchronize with others")]
 
     private float current;
-    [SerializeField] private float speedReference;
-
     private Vector3 startPos, endPos;
     private int iterator = 1;
     private int currentPatrolPos;
-    private float CurrentSpeed;
-    [SerializeField] private Vector3 direction;
+    private float length;
+    private float coefficient;
+    private Vector3 direction;
 
-    // private Rigidbody rb;
-
-
-    // Start is called before the first frame update
     void Start()
     {
-        //rb = GetComponent<Rigidbody>();
+        if (!isPigPong)
+        {
+            inverseDirectrion = false;
+        }
         current = deltaTime;
-        speedReference = referenceLength / speedMove;
         if (patrolPoints.Count > 1)
         {
             startPos = transform.position;
@@ -66,7 +65,7 @@ public class Magnit : MonoBehaviour
 
     protected virtual void MoveEnemy()
     {
-        current += Time.fixedDeltaTime * CurrentSpeed;
+        current += Time.fixedDeltaTime * coefficient * speedMove;
         transform.position = Vector3.Lerp(startPos, endPos, moveCurve.Evaluate(current));
     }
 
@@ -85,13 +84,18 @@ public class Magnit : MonoBehaviour
                 iterator = 1;
             SetPositions();
         }
-        //else
-        //{
-        //    if (currentPatrolPos == patrolPoints.Count - 1)
-        //        currentPatrolPos = 0;
-        //    //SetPositions();
-        //}
-
+        else
+        {
+            current = 0;
+            startPos = patrolPoints[currentPatrolPos].position;
+            if (currentPatrolPos == patrolPoints.Count - 1)
+                currentPatrolPos = 0;
+            else
+                currentPatrolPos += iterator;
+            endPos = patrolPoints[currentPatrolPos].position;
+            GetDirection();
+            GetCurrentSpeed();
+        }
 
     }
 
@@ -108,10 +112,8 @@ public class Magnit : MonoBehaviour
 
     private void GetCurrentSpeed()
     {
-        
-        float length = Vector3.Distance(startPos, endPos);
-
-        CurrentSpeed = (length / speedMove) /speedReference;
+        length = Vector3.Distance(startPos, endPos);
+        coefficient = referenceLength / length;
     }
 
     private void GetDirection()
@@ -125,31 +127,58 @@ public class Magnit : MonoBehaviour
         Debug.Log("Throw " + other);
         if (other.GetComponent<HookController>() != null)
         {
-            var player = other.GetComponent<HookController>();
-            player.SetBehaviorStan();
-            StartCoroutine(PushObject(direction, player.rb));
-            //player.rb.AddForce(direction * forceToPushObject, ForceMode.Impulse);
-            //other.GetComponent<Rigidbody>().AddForce(direction * forceToThrowObject, ForceMode.Impulse);
+            PushPlayer(other);
+            return;
         }
-        if (other.CompareTag("Enemy"))
+        if (other.GetComponent<IGrabable>() != null)
         {
+            var obj = other.GetComponent<IGrabable>();
+            if (other.GetComponent<Rigidbody>() != null)
+            {
+                Debug.Log(2);
+                var rb = other.GetComponent<Rigidbody>();
+                obj.OnGrab();
+                Debug.Log(3);
+                StartCoroutine (ReleaseObject(obj));
+                StartCoroutine (PushObject(direction, rb));
+                //rb.AddForce(direction * forceToPushObject, ForceMode.Impulse);
 
+                //PushObject(direction, rb);
+
+            }
+            else
+                Debug.Log("Objech Has no rigidbody");
         }
+    }
+
+    private IEnumerator ReleaseObject(IGrabable _object)
+    {
+        yield return new WaitForSeconds(timeStunEnemy);
+        _object.OnRelease();
+        StopCoroutine(ReleaseObject(_object));
+    }
+
+    private void PushPlayer(Collider other)
+    {
+        var player = other.GetComponent<HookController>();
+        player.SetBehaviorStan();
+        StartCoroutine(PushObject(direction, player.rb));
     }
 
     private IEnumerator PushObject(Vector3 direction, Rigidbody rb)
     {
+        Debug.Log(4);
         var startTime = Time.time;
         while (Time.time < startTime + duration)
         {
             var t = (Time.time - startTime) / duration;
-            Debug.Log(t);
             rb.velocity = direction * forceToPushObject * forceCurve.Evaluate(t);
             yield return new WaitForFixedUpdate();
         }
 
         if (StopMovingAfterFinished)
             rb.angularVelocity = rb.velocity = Vector3.zero;
+
         StopCoroutine(PushObject(direction, rb));
     }
 }
